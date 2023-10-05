93e73f3317d45b45f0b8248606faed6bd2551cba
MahmoudGh01 <mahmoud.gharbi@esprit.tn>
2023-10-05 11:47:32 +0100

    Completed

--- app/src/main/java/tn/esprit/nascar/MainActivity.kt
+++ app/src/main/java/tn/esprit/nascar/MainActivity.kt
@@ -2,6 +2,9 @@ package tn.esprit.nascar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import tn.esprit.nascar.databinding.ActivityMainBinding
 
@@ -15,22 +18,69 @@ class MainActivity : AppCompatActivity() {
         binding =ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         //TODO 11 Bind the toolbar to the activity
+        val toolbar = binding.toolbarLayout.toolbar
+        setSupportActionBar(toolbar)
 
-        //TODO 1 Implement the click on the 3 buttons (btnNews|btnEvents|btnProfile) to call changeFragment(...)
 
+        binding.btnNews.setOnClickListener {
+            changeFragment(NewsFragment(), "NewsFragment")
+        }
+
+        binding.btnEvents.setOnClickListener {
+            changeFragment(EventsFragment(), "EventsFragment")
+        }
+
+        binding.btnProfile.setOnClickListener {
+            changeFragment(ProfileFragment(), "ProfileFragment")
+        }
         //TODO 2 Implement the first call of the first fragment
+        val initialFragment = NewsFragment() // Replace with the fragment you want to show initially
+        val initialTag = "NewsFragment" // Replace with the tag for the initial fragment
+        changeFragment(initialFragment, initialTag)
     }
 
     private fun changeFragment(fragment: Fragment, tag: String) {
-
         //TODO 3 Check if tag is empty then replace the new fragment in the activity.
         //    If tag is not empty replace the new fragment in the activity and use addToBackStack
 
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment, tag)
+
+        if (!tag.isEmpty()) {
+            transaction.addToBackStack(tag)
+        }
+
+        transaction.commit()
     }
 
+
     //TODO 12 Override the method onCreateOptionsMenu()
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return true
+    }
+
 
     //TODO 13 Override the method onOptionsItemSelected() and Implement info and logout behavior
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.infoItem -> {
+                // Handle the infoItem click here
+                // Example: Show a toast message
+                Toast.makeText(this, "Info clicked", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.logoutItem -> {
+                // Handle the logoutItem click here
+                // Example: Perform logout action
+                // Replace this with your actual logout logic
+                finish() // Close the activity for demonstration purposes
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
 
 }
--- app/src/main/java/tn/esprit/nascar/NewsFragment.kt
+++ app/src/main/java/tn/esprit/nascar/NewsFragment.kt
@@ -1,7 +1,24 @@
 package tn.esprit.nascar
 
-//TODO 4 Change this class to a fragment and use binding to inflate fragment_news.xml
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import tn.esprit.nascar.databinding.FragmentNewsBinding
 
-class NewsFragment {
+class NewsFragment : Fragment() {
 
+    private lateinit var binding: FragmentNewsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment using data binding
+        binding = FragmentNewsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    // Add any fragment-specific logic here
 }
